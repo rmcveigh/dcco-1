6a3bed8a9046b7fa00485ed204b3325f8d109480
danomanion <danomanion@375742.no-reply.drupal.org>
2014-06-10 13:33:54 -0700

    Issue #2275757 by danomanion, neardark | MatthewS: Front End Developers and an Austin BOF for sprinting?.

--- profiles/dcco/themes/dcco2014/js/main.js
+++ profiles/dcco/themes/dcco2014/js/main.js
@@ -1,5 +1,10 @@
 (function ($) {
   $(document).ready(function(){
-
+    // Add icons to specific links in the main menu
+    var $mainMenu = $('#block-system-main-menu');
+    $mainMenu.find('.sessions a').addClass('icon-calendar');
+    $mainMenu.find('.updates a').addClass('icon-comment');
+    $mainMenu.find('.sponsors a').addClass('icon-heart');
+    $mainMenu.find('.travel a').addClass('icon-plane');
   });
 })(jQuery);
