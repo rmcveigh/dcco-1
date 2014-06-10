be767f8b9a52d324727d76c11768a39213fbeb32
Ryan McVeigh <ryanm@newmediadenver.com>
2014-06-10 16:15:11 -0600

    styled the primary menus on the site

--- profiles/dcco/themes/dcco2014/js/menu.js
+++ profiles/dcco/themes/dcco2014/js/menu.js
@@ -2,9 +2,13 @@
   Drupal.behaviors.emplementMenu = {
     attach: function (context, settings) {
       $('#page', context).once('asdfasdf', function(){
-        var mainNav = responsiveNav('block-system-main-menu');
+        var mainNav = responsiveNav('block-system-main-menu', {
+          label: '<span class="mobile-main-menu">Main Menu</span>'
+        });
         if ($('#block-system-user-menu', context).length) {
-          var userNav = responsiveNav('block-system-user-menu');
+          var userNav = responsiveNav('block-system-user-menu', {
+            label: '<span class="mobile-user-menu">User Menu</span>'
+          });
         };
       });
     }
