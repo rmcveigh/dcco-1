b7acaf1b09f6dd71ce3da22153d371def3bab780
neardark <neardark@661076.no-reply.drupal.org>
2014-06-11 19:22:03 -0600

    Issue #2284759 by neardark: Theme - Fix nasty slideshow circle thing. .

--- profiles/dcco/themes/dcco2014/js/main.js
+++ profiles/dcco/themes/dcco2014/js/main.js
@@ -7,8 +7,6 @@
       $mainMenu.find('.updates a').addClass('icon-comment');
       $mainMenu.find('.sponsors a').addClass('icon-heart');
       $mainMenu.find('.travel a').addClass('icon-plane');
-      
-      stylePromoImage();
 
       // Activate Slick carousel on landing page promo.
       $('.promos__wrapper').slick({
@@ -17,19 +15,5 @@
           autoplay: true,
           autoplaySpeed: 5000
       });
-
-      /**
-       * Add an inline style tag for the background promo image, based off of its
-       * data-promo-background attribute because views disallows style tags on field
-       * rewrites for security purposes.
-       */
-      function stylePromoImage() {
-          var $promos = $('.promo__image a');
-          $.each($promos, function () {
-              var promoImage = $(this)[0].attributes;
-              var promoBackground = $(this)[0].attributes[0].nodeValue;
-              $(this).attr('style', 'background-image: url(' + promoBackground + ')');
-          });
-      }
   });
 })(jQuery);
